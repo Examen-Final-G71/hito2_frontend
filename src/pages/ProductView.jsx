f38b48970d23cc55dedf6c880b7a5be5e54c16d1
Fleepx <166570783+Fleepx@users.noreply.github.com>
2025-02-25 12:15:29 -0300

    Ver publicaciones en ProductView.jsx

--- src/pages/ProductView.jsx
+++ src/pages/ProductView.jsx
@@ -5,13 +5,52 @@ import { AppContext } from '../context/AppContext';
 
 function ProductView() {
   const location = useLocation();
+  const product = location.state?.product;
+  const { addToCart } = useContext(AppContext);
+
+  if (!product) {
+    return <Container className="mt-5"><p>Producto no encontrado.</p></Container>;
+  }
+
+  return (
+    <Container className="mt-5" style={{ marginBottom: '80px' }}>
+      <Row>
+        <Col md={6}>
+          <Image src={product.imagen} alt={product.nombre} fluid rounded style={{ maxHeight: '500px', objectFit: 'contain' }} />
+        </Col>
+        <Col md={6} className="d-flex flex-column justify-content-center">
+          <h1>{product.nombre}</h1>
+          <p>{product.descripcion}</p>
+          <p>Precio: ${product.precio}</p>
+          <Button variant="primary" onClick={() => addToCart(product)}>
+            Añadir al Carrito
+          </Button>
+          <Link to="/gallery">
+            <Button variant="outline-primary" className="mt-3">Volver a la Galería</Button>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default ProductView;
+
+
+/*import React, { useContext } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import { AppContext } from '../context/AppContext';
+
+function ProductView() {
+  const location = useLocation();
   const product = location.state.product;
   const { addToCart } = useContext(AppContext);
 
   return (
     <Container className="mt-5" style={{ marginBottom: '80px' }}>
         <Col>
-          <Link to="/gallery"> {/* Enlace para volver a la galería */}
+          <Link to="/gallery">
             <Button variant="outline-primary">Volver</Button>
           </Link>
         </Col>
@@ -22,7 +61,7 @@ function ProductView() {
         <Col md={6} className="d-flex flex-column justify-content-center">
           <h1>{product.title}</h1>
           <p>{product.description}</p>
-          <p>Precio: ${product.price}</p> {/* Mostrar el precio */}
+          <p>Precio: ${product.price}</p> 
           <button
                 className="btn btn-primary"
                 onClick={() => addToCart(product)} 
@@ -36,4 +75,4 @@ function ProductView() {
   );
 }
 
-export default ProductView;
+export default ProductView;*/
