0294597b369aa475e09e202b947de76db02a8d04
karu5 <fabiolakmschmidt@gmail.com>
2025-03-04 23:19:43 -0300

    cambio en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,26 +8,26 @@ const Footer = () => {
         <footer className="bg-dark text-light py-4 custom-footer">
             <Container className='pt-7'>
                 <Row>
-                <Col md={5}>
-  <h5>Síguenos</h5>
-  <ul className="list-unstyled d-flex"> 
-    <li className="me-3"> 
-      <a href="" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faFacebookF} size="lg" style={{ color: '#FCB432' }} /> 
-      </a>
-    </li>
-    <li className="me-3">
-      <a href="" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faTwitter} size="lg" style={{ color: '#FCB432' }} />
-      </a>
-    </li>
-    <li>
-      <a href="" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faInstagram} size="lg" style={{ color: '#FCB432' }} />
-      </a>
-    </li>
-  </ul>
-</Col>
+                    <Col md={4}>
+                        <h5>Síguenos</h5>
+                        <ul className="list-unstyled d-flex"> 
+                            <li className="me-3"> 
+                                <a href="" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faFacebookF} size="lg" style={{ color: '#FCB432' }} /> 
+                                </a>
+                            </li>
+                            <li className="me-3">
+                                <a href="" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faTwitter} size="lg" style={{ color: '#FCB432' }} />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faInstagram} size="lg" style={{ color: '#FCB432' }} />
+                                </a>
+                            </li>
+                        </ul>
+                    </Col>
 
                     <Col md={4}>
                         <h5>Contacto</h5>
@@ -37,7 +37,17 @@ const Footer = () => {
                             <li><i className="bi bi-envelope-fill"></i> Email: soporte@iu.com</li>
                         </ul>
                     </Col>
+
+                    <Col md={4}>
+                        <h5>Información</h5>
+                        <ul className="list-unstyled">
+                            <li><a href="/quienes-somos" className="text-light">¿Quiénes somos?</a></li>
+                            <li><a href="/terminos-condiciones" className="text-light">Términos y condiciones</a></li>
+                            <li><a href="/ayuda" className="text-light">Ayuda</a></li>
+                        </ul>
+                    </Col>
                 </Row>
+                
                 <Row className="mt-3">
                     <Col md={12} className="text-center">
                         <p className="mb-0">&copy; 2025 Felipe Sánchez, Verónica Gática y Fabiola Muñoz. Todos los derechos reservados.</p>
@@ -49,3 +59,8 @@ const Footer = () => {
 };
 
 export default Footer;
+
+
+
+
+
